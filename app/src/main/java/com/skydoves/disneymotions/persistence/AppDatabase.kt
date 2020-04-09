919ed52a9b46dd043bb9461a7f4a03829e09bb0c
skydoves <skydoves@naver.com>
2020-04-10 02:00:09 +0900

    Export room persistence scheme

--- app/src/main/java/com/skydoves/disneymotions/persistence/AppDatabase.kt
+++ app/src/main/java/com/skydoves/disneymotions/persistence/AppDatabase.kt
@@ -20,7 +20,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.skydoves.disneymotions.model.Poster
 
-@Database(entities = [Poster::class], version = 1, exportSchema = false)
+@Database(entities = [Poster::class], version = 1, exportSchema = true)
 abstract class AppDatabase : RoomDatabase() {
 
   abstract fun posterDao(): PosterDao
