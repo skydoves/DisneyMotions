651640ca842ed64bbe5f3c5ef3409bcd43eb3bc2
skydoves <skydoves@naver.com>
2020-06-11 02:50:45 +0900

    Bump okHttp and retrofit dependency versions

--- app/src/main/java/com/skydoves/disneymotions/view/ui/main/HomeFragment.kt
+++ app/src/main/java/com/skydoves/disneymotions/view/ui/main/HomeFragment.kt
@@ -16,27 +16,64 @@
 
 package com.skydoves.disneymotions.view.ui.main
 
+import android.graphics.Color
 import android.os.Bundle
+import android.transition.TransitionManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.transition.platform.MaterialArcMotion
+import com.google.android.material.transition.platform.MaterialContainerTransform
 import com.skydoves.disneymotions.R
 import com.skydoves.disneymotions.base.DatabindingFragment
 import com.skydoves.disneymotions.databinding.FragmentHomeBinding
+import com.skydoves.disneymotions.extensions.gone
+import com.skydoves.disneymotions.extensions.visible
 import com.skydoves.disneymotions.view.adapter.PosterAdapter
 import org.koin.android.viewmodel.ext.android.getViewModel
 
 class HomeFragment : DatabindingFragment() {
+
+  private lateinit var binding: FragmentHomeBinding
 
   override fun onCreateView(
     inflater: LayoutInflater,
     container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View? {
-    return binding<FragmentHomeBinding>(inflater, R.layout.fragment_home, container).apply {
+    binding = binding<FragmentHomeBinding>(inflater, R.layout.fragment_home, container).apply {
       viewModel = getViewModel<MainViewModel>().apply { fetchDisneyPosterList() }
       lifecycleOwner = this@HomeFragment
       adapter = PosterAdapter()
-    }.root
+    }
+    return binding.root
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+
+    binding.fab.setOnClickListener {
+      TransitionManager.beginDelayedTransition(binding.container,
+        getTransform(it, binding.card))
+      binding.card.visible()
+      it.gone()
+    }
+
+    binding.card.setOnClickListener {
+      TransitionManager.beginDelayedTransition(binding.container,
+        getTransform(it, binding.fab))
+      binding.fab.visible()
+      it.gone()
+    }
+  }
+
+  internal fun getTransform(mStartView: View, mEndView: View): MaterialContainerTransform {
+    return MaterialContainerTransform().apply {
+      startView = mStartView
+      endView = mEndView
+      pathMotion = MaterialArcMotion()
+      duration = 550
+      scrimColor = Color.TRANSPARENT
+    }
   }
 }
