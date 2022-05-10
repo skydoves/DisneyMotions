87fab5020ee30822cefc40fb611d50280d6fdb26
skydoves <skydoves@naver.com>
2022-05-10 21:24:31 +0900

    Bump dependencies

--- app/src/main/java/com/skydoves/disneymotions/di/NetworkModule.kt
+++ app/src/main/java/com/skydoves/disneymotions/di/NetworkModule.kt
@@ -18,7 +18,7 @@ package com.skydoves.disneymotions.di
 
 import com.skydoves.disneymotions.network.DisneyService
 import com.skydoves.disneymotions.network.RequestInterceptor
-import com.skydoves.sandwich.coroutines.CoroutinesResponseCallAdapterFactory
+import com.skydoves.sandwich.adapters.ApiResponseCallAdapterFactory
 import okhttp3.OkHttpClient
 import org.koin.dsl.module
 import retrofit2.Retrofit
@@ -39,7 +39,7 @@ val networkModule = module {
         "https://gist.githubusercontent.com/skydoves/aa3bbbf495b0fa91db8a9e89f34e4873/raw/a1a13d37027e8920412da5f00f6a89c5a3dbfb9a/"
       )
       .addConverterFactory(GsonConverterFactory.create())
-      .addCallAdapterFactory(CoroutinesResponseCallAdapterFactory.create())
+      .addCallAdapterFactory(ApiResponseCallAdapterFactory.create())
       .build()
   }
 
